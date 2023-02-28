d2076acc12e1167a56db5e269ed1a3ecf6d5e344
aashish8174 <118752988+aashish8174@users.noreply.github.com>
2023-02-28 17:15:11 +0530

    Add files via upload

---
+++ airline_management_system/Airline_management_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline_management_system;
+
+/**
+ *
+ * @author Aashish Kumar Yadav
+ */
+public class Airline_management_system {
+
+    public static void main(String[] args) {
+  
+    }
+}
